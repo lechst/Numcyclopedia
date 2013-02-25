38d27f9188496f41930fc7b9b373efbba63d1119
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-25 22:03:07 +0100

    pentagonal i centered pentagonal

---
+++ js/sequences/centeredPentagonalSequence.class.js
@@ -0,0 +1,22 @@
+function centeredPentagonalSequence() {
+    Sequence.call(this);
+}
+
+centeredPentagonalSequence.prototype = new Sequence();
+
+centeredPentagonalSequence.prototype.constructor = centeredPentagonalSequence;
+
+centeredPentagonalSequence.prototype.name = 'centered pentagonal';
+
+centeredPentagonalSequence.prototype.length = Infinity;
+
+centeredPentagonalSequence.prototype.Q = function (n){
+
+    var x = Math.round((Math.sqrt(5)*Math.sqrt(8*x-3)-5)/10);
+
+    if(((5*x*x+5*x+2)/2) == n){
+        return true;
+    }
+
+    return false;
+}
---
+++ js/sequences/pentagonalSequence.class.js
@@ -0,0 +1,22 @@
+function pentagonalSequence() {
+    Sequence.call(this);
+}
+
+pentagonalSequence.prototype = new Sequence();
+
+pentagonalSequence.prototype.constructor = pentagonalSequence;
+
+pentagonalSequence.prototype.name = 'pentagonal';
+
+pentagonalSequence.prototype.length = Infinity;
+
+pentagonalSequence.prototype.Q = function (n){
+
+    var x = Math.round((1+Math.sqrt(1+24*x))/6);
+
+    if((x*(3*x-1)/2) == n){
+        return true;
+    }
+
+    return false;
+}
