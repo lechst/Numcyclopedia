59b45f64a9026e3c52cd859b670a0862c4aa0899
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-25 20:47:56 +0100

    triangularSequence

---
+++ js/sequences/triangularSequence.class.js
@@ -0,0 +1,22 @@
+function triangularSequence() {
+    Sequence.call(this);
+}
+
+triangularSequence.prototype = new Sequence();
+
+triangularSequence.prototype.constructor = triangularSequence;
+
+triangularSequence.prototype.name = 'square';
+
+triangularSequence.prototype.length = Infinity;
+
+triangularSequence.prototype.Q = function (n){
+
+    var x = Math.floor((Math.sqrt(8*n+1)-1)/2);
+
+    if((x*(x+1)/2) == n){
+        return true;
+    }
+
+    return false;
+}
