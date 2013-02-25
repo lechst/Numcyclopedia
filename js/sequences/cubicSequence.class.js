612efc1cdaf3202e8d749b29987731ed9a57419d
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-25 19:31:34 +0100

    cubic number sequence

---
+++ js/sequences/cubicSequence.class.js
@@ -0,0 +1,22 @@
+function cubicSequence() {
+    Sequence.call(this);
+}
+
+cubicSequence.prototype = new Sequence();
+
+cubicSequence.prototype.constructor = cubicSequence;
+
+cubicSequence.prototype.name = 'cube';
+
+cubicSequence.prototype.length = Infinity;
+
+cubicSequence.prototype.Q = function (n){
+
+    var x = Math.floor(Math.pow(n,1/3));
+
+    if(x*x*x == n){
+        return true;
+    }
+
+    return false;
+}
