533988185fc74fa16a89b1d49ffe6570e80d0d09
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-26 11:42:18 +0100

    biquadratic sequence

---
+++ js/sequences/biquadraticSequence.class.js
@@ -0,0 +1,22 @@
+function biquadraticSequence() {
+    Sequence.call(this);
+}
+
+biquadraticSequence.prototype = new Sequence();
+
+biquadraticSequence.prototype.constructor = biquadraticSequence;
+
+biquadraticSequence.prototype.name = 'biquadratic';
+
+biquadraticSequence.prototype.length = Infinity;
+
+biquadraticSequence.prototype.Q = function (n){
+
+    var x = Math.round(Math.pow(n,1/4));
+
+    if(x*x*x*x == n){
+        return true;
+    }
+
+    return false;
+}
