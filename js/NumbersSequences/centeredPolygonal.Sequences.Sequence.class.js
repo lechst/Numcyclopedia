960e1c702e9c471bfad078d1dace561a17156814
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-27 12:43:45 +0100

    dodane ciagi polygonal i centered polygonal

---
+++ js/NumbersSequences/centeredPolygonal.Sequences.Sequence.class.js
@@ -0,0 +1,101 @@
+function centeredPolygonalSequencesSequence() {
+    Sequence.call(this);
+}
+
+centeredPolygonalSequencesSequence.prototype = new Sequence();
+
+centeredPolygonalSequencesSequence.prototype.constructor = centeredPolygonalSequencesSequence;
+
+centeredPolygonalSequencesSequence.prototype.name = 'centered polygonal numbers';
+
+centeredPolygonalSequencesSequence.prototype.length = Infinity;
+
+centeredPolygonalSequencesSequence.prototype.getN = function(n){
+
+    return new centeredPolygonalSequence(n);
+
+};
+
+
+
+
+function centeredPolygonalSequence(n) {
+
+    NumbersSequence.call(this);
+
+    if(!n) {
+        throw new Error("n must be specified!")
+    }
+
+    this.N = n;
+
+}
+
+centeredPolygonalSequence.prototype = new NumbersSequence();
+
+centeredPolygonalSequence.prototype.constructor = centeredPolygonalSequence;
+
+centeredPolygonalSequence.prototype.length = Infinity;
+
+centeredPolygonalSequence.prototype.getN = function(n){
+
+    var r = this.N + 2;
+    var x = (r*n*(n-1))/2+1;
+
+    return x;
+
+};
+
+centeredPolygonalSequence.prototype.__defineGetter__('name',function(){
+    var r = this.N + 2;
+
+    return "centered "+r+"-gonal";
+});
+
+centeredPolygonalSequence.prototype.Q = function (n){
+
+    var r = this.N + 2;
+
+    var x = Math.round((1+Math.sqrt(8*n+r-8)/Math.sqrt(r))/2);
+
+    if(((r*x*(x-1))/2+1) == n){
+        return true;
+    }
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
