36398b9d52e4a730d88888a37ce19151db9ac2f8
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-02-28 12:16:00 +0100

    graficzne przedstawienie ciagu Fibonacciego

--- js/NumbersSequences/fibonacciSequence.class.js
+++ js/NumbersSequences/fibonacciSequence.class.js
@@ -25,3 +25,53 @@ fibonacciSequence.prototype.Q = function (n){
 
     return false;
 }
+
+fibonacciSequence.prototype.N = function (n){
+
+    var a = 1;
+    var b = 1;
+
+    var x = 0;
+
+    for(var i=0; i<n; i++){
+        x = a + b;
+        a = b;
+        b = x;
+    }
+
+    return x;
+}
+
+fibonacciSequence.prototype.arrange = function(n){
+
+    var pos = {};
+    pos.x = [];
+    pos.y = [];
+
+    pos.x.push(0);
+    pos.y.push(0);
+
+    pos.x.push(1);
+    pos.y.push(0);
+
+    var x = 1;
+    var y = 0;
+    var alpha = -2*Math.PI/8;
+
+    for(var i=0; i<n; i++){
+        pos.x.push(x-Math.sin(alpha+2*Math.PI/8));
+        pos.y.push(y-Math.cos(alpha+2*Math.PI/8));
+        x = pos.x[pos.x.length-1];
+        y = pos.y[pos.y.length-1];
+        for(var j=0; j<fibonacciSequence.prototype.N(i+1)-1; j++){
+            pos.x.push(x-(j+1)*Math.sqrt(2)*Math.cos(alpha));
+            pos.y.push(y+(j+1)*Math.sqrt(2)*Math.sin(alpha));
+        }
+        x = pos.x[pos.x.length-1];
+        y = pos.y[pos.y.length-1];
+        alpha = alpha + 2*Math.PI/4;
+    }
+
+    return pos;
+
+};
