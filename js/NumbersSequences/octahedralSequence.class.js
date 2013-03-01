24fe9eb8f6f18c0ee2afb16247f1145adedfe20a
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-03-01 19:31:53 +0100

    grafika dla octahedral numbers

--- js/NumbersSequences/octahedralSequence.class.js
+++ js/NumbersSequences/octahedralSequence.class.js
@@ -31,4 +31,73 @@ octahedralSequence.prototype.Q = function (n){
     return false;
 }
 
-console.log(octahedralSequence.prototype.Q(85));
+octahedralSequence.prototype.arrange3D = function(n){
+
+    var pos = {};
+    pos.x = [];
+    pos.y = [];
+    pos.z = [];
+
+    var r = 4;
+    var alpha = 2*Math.PI/r;
+
+    var x = 0;
+    var y = 0;
+    var z = 0;
+    var dx = 0;
+    var dy = 0;
+
+    for(var m=n; m>0; m--){
+
+        x = 0 + dx;
+        y = 0 + dy;
+        z = n - m;
+
+        pos.x.push(x);
+        pos.y.push(y);
+        pos.z.push(z);
+
+        pos.x.push(x);
+        pos.y.push(y);
+        pos.z.push(-z);
+
+        for(var i=1; i<m; i++){
+            x = i + dx;
+            y = 0 + dy;
+
+            pos.x.push(x);
+            pos.y.push(y);
+            pos.z.push(z);
+
+            pos.x.push(x);
+            pos.y.push(y);
+            pos.z.push(-z);
+
+            for(var j=0; j<(r-2); j++){
+                for(var k=0; k<i; k++){
+                    pos.x.push(x+(k+1)*Math.cos(alpha));
+                    pos.y.push(y-(k+1)*Math.sin(alpha));
+                    pos.z.push(z);
+
+                    pos.x.push(x+(k+1)*Math.cos(alpha));
+                    pos.y.push(y-(k+1)*Math.sin(alpha));
+                    pos.z.push(-z);
+                }
+                x = pos.x[pos.x.length-1];
+                y = pos.y[pos.y.length-1];
+                alpha = alpha + 2*Math.PI/r;
+            }
+
+            alpha = 2*Math.PI/r;
+        }
+
+        dx = dx + 1/2;
+        dy = dy - 1/(2*Math.tan(alpha/2));
+
+    }
+
+    return pos;
+
+};
+
+octahedralSequence.prototype.arrange3D.fill = true;
