5924f6ee5448d8d48aaba6697624e9a04aa4cded
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-03-04 12:41:50 +0100

    dodana funkcja 'divisors' do pliku 'utils'

--- js/utils.js
+++ js/utils.js
@@ -121,6 +121,25 @@ var divisorFunction = function(n){
     return x;
 }
 
+var divisors = function(n){
+    var a = factors(n);
+    var d = [1];
+
+    for(var i=0; i<a.length; i++){
+        var l = d.length;
+
+        for(var j=0; j<l; j++){
+            for(var k=1; k<(a[i].power+1); k++){
+                d.push(d[j]*Math.pow(a[i].prime,k));
+            }
+        }
+    }
+
+    d = d.sort(function(a,b){return a-b});
+
+    return d;
+}
+
 var digits = function(n){
 
     var a = [];
