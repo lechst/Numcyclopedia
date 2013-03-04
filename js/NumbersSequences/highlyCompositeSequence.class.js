714b01b2e99f22303883066b625fc7764b499946
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-03-04 18:03:16 +0100

    dodany highlyCompositeSequence

---
+++ js/NumbersSequences/highlyCompositeSequence.class.js
@@ -0,0 +1,35 @@
+function highlyCompositeSequence() {
+    NumbersSequence.call(this);
+}
+
+highlyCompositeSequence.prototype = new NumbersSequence();
+
+highlyCompositeSequence.prototype.constructor = highlyCompositeSequence;
+
+highlyCompositeSequence.prototype.name = 'highly composite';
+
+highlyCompositeSequence.prototype.wiki = "http://mathworld.wolfram.com/Highly_composite_number";
+highlyCompositeSequence.prototype.wolfram = "http://mathworld.wolfram.com/HighlyCompositeNumber.html";
+
+highlyCompositeSequence.prototype.length = Infinity;
+
+highlyCompositeSequence.prototype.Q = function (n){
+
+    var m = divisorsN(n);
+    var k = 0;
+
+    if(n == 2){
+        return true;
+    }
+
+    for(var i=1; i<n; i++){
+        k = divisorsN(i);
+        if(k >= m){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(divisorsN(1));
