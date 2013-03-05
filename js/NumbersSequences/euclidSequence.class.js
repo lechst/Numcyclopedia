dee7404abf9798c7ec6231ba2eead3a4b54936ad
Lech Stawikowski <lech.stawikowski@gmail.com>
2013-03-05 18:13:26 +0100

    dodany euclidSequence

---
+++ js/NumbersSequences/euclidSequence.class.js
@@ -0,0 +1,67 @@
+function euclidSequence() {
+    NumbersSequence.call(this);
+}
+
+euclidSequence.prototype = new NumbersSequence();
+
+euclidSequence.prototype.constructor = euclidSequence;
+
+euclidSequence.prototype.name = 'Euclid';
+
+euclidSequence.prototype.wiki = "http://en.wikipedia.org/wiki/Euclid_number";
+euclidSequence.prototype.wolfram = "http://mathworld.wolfram.com/EuclidNumber.html";
+
+euclidSequence.prototype.texExpression = "E_n=1+\\prod_{i=1}^{n}p_i";
+
+euclidSequence.prototype.texExpressionForN = function(x,n){
+
+    var m = this.getN(n);
+
+    var expr = m+'=1+';
+
+    for(var i=1; i<n; i++){
+        expr += primesSequence.prototype.getN(i)+'\\cdot';
+    }
+
+    expr += primesSequence.prototype.getN(n);
+
+    return expr;
+
+}
+
+euclidSequence.prototype.length = Infinity;
+
+euclidSequence.prototype.getN = function(n){
+
+    var x = 1;
+
+    for(var i=1; i<=n; i++){
+        x *= primesSequence.prototype.getN(i);
+    }
+
+    x += 1;
+
+    return x;
+
+}
+
+euclidSequence.prototype.Q = function (n){
+
+    if(n==1){
+        return false;
+    }
+
+    var x = 1;
+    var i = 1;
+
+    while(x < n){
+        x = euclidSequence.prototype.getN(i);
+        i++;
+    }
+
+    if(x == n){
+        return true;
+    }
+
+    return false;
+}
